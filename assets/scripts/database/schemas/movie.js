5661dfa206cb238d57a56125b507b567f337f9bb
Gustavo Rodrigues <88632400+zmaderow@users.noreply.github.com>
2023-09-18 15:51:38 -0300

    ♻️Corrections

---
+++ assets/scripts/database/schemas/movie.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let movieSchema = new Schema({
+    mID: { type: Number },
+    mName: { type: String },
+    mSinopse: { type: String },
+    mDuration: { type: Number },
+    mType: { type: String },
+    mGenres: { type: Array },
+    mFullSizeCover: { type: String },
+    mCover: { type: String },
+    mEpisodes: { type: Array },
+    // mEpisodes: { epNumber: 0, epName: " ", epURL: " ", epThumbnail: " "}
+})
+
+let Movie = mongoose.model("Movie", movieSchema);
+module.exports = Movie;
