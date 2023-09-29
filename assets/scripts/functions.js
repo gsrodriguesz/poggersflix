0153c44a1a681436a968eabf79c8bcd4d6a57e9d
Gustavo Rodrigues <88632400+zmaderow@users.noreply.github.com>
2023-09-28 22:53:43 -0300

    🚩 deleting directories

--- assets/scripts/functions.js
+++
@@ -1,8 +0,0 @@
-function showNav() {
-    navElement = document.getElementById("navContent")
-    if (navElement.style.display == 'none') {
-        navElement.style.display = 'flex'
-    } else {
-        navElement.style.display = 'none'
-    }
-}
